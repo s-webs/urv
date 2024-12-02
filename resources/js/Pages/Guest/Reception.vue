df4662ef048d4e2dc278223c2ec0c24321ffb03c
Nikita V <swebs.sh@gmail.com>
2024-12-02 17:30:53 +0500

    Изменил градиент на странице qr кода

--- resources/js/Pages/Guest/Reception.vue
+++ resources/js/Pages/Guest/Reception.vue
@@ -3,7 +3,7 @@ import QrcodeVue from 'qrcode.vue'
 </script>
 
 <template>
-    <div class="h-screen w-screen overflow-hidden bg-gradient-to-r from-yellow-200 to-green-300 flex items-center justify-center">
+    <div class="h-screen w-screen overflow-hidden bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center">
         <div class="bg-white w-2/4 h-2/4 flex items-center justify-center rounded-lg box-border">
             <QrcodeVue :value="route('page.check')" :size="320"/>
         </div>
