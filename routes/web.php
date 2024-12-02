7310b2c7b51879cb15030d57b6e8fe4182d5c3f0
Nikita V <swebs.sh@gmail.com>
2024-12-02 16:41:10 +0500

    Добавил страницу с qr кодом

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 
-Route::get('reception', [\App\Http\Controllers\GuestController::class, 'reception'])->name('reception');
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [\App\Http\Controllers\GuestController::class, 'reception'])->name('reception');
 
 Route::middleware([
     'auth:sanctum',
