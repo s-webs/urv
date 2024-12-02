beb0f9576053c6c810569106a4b7b2dfcf26f194
Nikita V <swebs.sh@gmail.com>
2024-12-02 16:29:00 +0500

    Добавил ссылку на яндекс карты и добавил метки

--- resources/js/Pages/Guest/Check.vue
+++ resources/js/Pages/Guest/Check.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref } from 'vue';
-import { useForm } from '@inertiajs/vue3';
+import {ref} from 'vue';
+import {useForm} from '@inertiajs/vue3';
 
 const errorMessage = ref('');
 const successMessage = ref('');
-const mapLink = ref('');
+const mapLinkGoogle = ref('');
+const mapLinkYandex = ref('');
 
 const form = useForm({
     latitude: null,
@@ -18,7 +19,8 @@ const checkIn = () => {
                 form.latitude = position.coords.latitude;
                 form.longitude = position.coords.longitude;
                 console.log(form.latitude, form.longitude)
-                mapLink.value = `https://www.google.com/maps?ll=${form.latitude},${form.longitude}`;
+                mapLinkGoogle.value = `https://www.google.com/maps?q=${form.latitude},${form.longitude}`;
+                mapLinkYandex.value = `https://yandex.ru/maps/?pt=${form.longitude},${form.latitude}&z=18&l=map`;
             },
             (error) => {
                 console.error('Ошибка геолокации:', error);
@@ -27,7 +29,8 @@ const checkIn = () => {
         );
     } else {
         errorMessage.value = 'Ваше устройство не поддерживает геолокацию.';
-        mapLink.value = '';
+        mapLinkGoogle.value = '';
+        mapLinkYandex.value = '';
     }
 };
 
@@ -39,6 +42,7 @@ const checkIn = () => {
     color: red;
     margin-top: 10px;
 }
+
 .success {
     color: green;
     margin-top: 10px;
@@ -51,8 +55,13 @@ const checkIn = () => {
         <p>Для отметки прихода мы проверим ваше местоположение.</p>
         <button class="bg-green-600 p-4 mt-4" @click="checkIn">Отметить приход</button>
 
-        <p v-if="mapLink">
-            <a :href="mapLink" target="_blank">Открыть местоположение на карте</a>
+        <p v-if="mapLinkGoogle" class="mt-4">
+            <span class="block">Google</span>
+            <a :href="mapLinkGoogle" target="_blank" class="text-blue-600 border">Открыть местоположение на карте</a>
+        </p>
+        <p v-if="mapLinkYandex" class="mt-4">
+            <span class="block">Yandex</span>
+            <a :href="mapLinkYandex" target="_blank" class="text-blue-600 border">Открыть местоположение на карте</a>
         </p>
     </div>
 </template>
